4ee916c4ae75a8cb0dde74205a0105ece9ad5cf9
Harald Kirschner <hkirschner@mozilla.com>
2014-03-05 13:48:07 -0800

    Config cleanup

--- config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var path = require("path");
-
-module.exports = {
-  derFilePath: path.join(__dirname, "config-for-phone.der"),
-  keystorePath: path.join(__dirname, "my-d2g-keystore.ks"),
-};
