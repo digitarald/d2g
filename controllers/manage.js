5d4223a3ccfbb39cbe9abff53c6ff6d305f44dec
Harald Kirschner <hkirschner@mozilla.com>
2014-03-04 16:48:51 -0800

    Standalone for manage page to trigger Dashboard React page

--- controllers/manage.js
+++ controllers/manage.js
@@ -9,58 +9,50 @@ var User = require('../models/User');
 var Version = require('../models/version');
 
 exports.getIndex = function(req, res) {
-  res.render('manage/index', {
-    title: 'Dashboard'
-  });
-};
-
-exports.getNewProject = function(req, res) {
-  res.render('manage/new-project', {
-    title: 'New Project'
-  });
-};
+	res.render('manage/index', {
+		title: 'Dashboard',
+		standalone: true
+	});
 
 exports.uploadApp = function(req, res) {
-  if (! req.files ||
-      ! req.files.zip ||
-      ! req.files.zip.path) {
-    return res.send(400, 'Bad upload');
-  }
-  owaReader(req.files.zip.path, function(err, name, version) {
-    if (err) {
-      console.log(err);
-      console.error(err);
-      return res.send(404, 'Unable to read app zip');
-    }
-    // TODO this should be based on Mongo
-    if (! version) {
-      version = new Date().getTime();
-    }
+	if (!req.files || !req.files.zip || !req.files.zip.path) {
+		return res.send(400, 'Bad upload');
+	}
+	owaReader(req.files.zip.path, function(err, name, version) {
+		if (err) {
+			console.log(err);
+			console.error(err);
+			return res.send(404, 'Unable to read app zip');
+		}
+		// TODO this should be based on Mongo
+		if (!version) {
+			version = new Date().getTime();
+		}
 
-  res.send('Great! ' + name + ' ' + version);
-  });
+		res.send('Great! ' + name + ' ' + version);
+	});
 };
 
 var _createProject = function(name, users, version, cb) {
-  users = user || [];
-  var aProject = new Project({
-    name: name,
-    users: users
-  });
+	users = user || [];
+	var aProject = new Project({
+		name: name,
+		users: users
+	});
 
-  aProject.save(function(err, newProject) {
-    if (err) {
-      return cb(err);
-    }
-    var aVersion = new Version({
-      version: version,
-      _project: aProject._id
-    });
-    aVersion.save(function(err, newVersion) {
-      if (err) {
-        return cb(err);
-      }
-      cb(null, newProject, newVersion);
-    });
-  });
+	aProject.save(function(err, newProject) {
+		if (err) {
+			return cb(err);
+		}
+		var aVersion = new Version({
+			version: version,
+			_project: aProject._id
+		});
+		aVersion.save(function(err, newVersion) {
+			if (err) {
+				return cb(err);
+			}
+			cb(null, newProject, newVersion);
+		});
+	});
 }
