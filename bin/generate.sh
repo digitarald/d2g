f209878fdbc3575aa70078e846ccc942e0add1d1
Bill Walker <bwalker@mozilla.com>
2014-03-06 14:44:31 -0800

    refactoring generate.sh -- remove unneeded replace_zip, split generating cert and signing app, etc

--- bin/generate.sh
+++ bin/generate.sh
@@ -9,30 +9,46 @@
 
 set -x
 
+# let the source directory be the current working directory
 srcdir=$PWD
+
+# create a temp dir, put a password file in there
 tmpdir=$TMP/test_signed_apps
-noisefile=$tmpdir/noise
 passwordfile=$tmpdir/passwordfile
-ca_responses=$tmpdir/ca_responses
-ee_responses=$tmpdir/ee_responses
 
-replace_zip()
-{
-  zip=$1 # must be an absolute path
-  new_contents_dir=$2
+# # add and overwrite the contents of the archive at $1 with anything found in directory $2
+# replace_zip()
+# {
+#   zip=$1 # must be an absolute path
+#   new_contents_dir=$2
 
-  # bwalker I had to comment this out
-  # rm -f $zip
-  oldpwd=$PWD
-  cd $new_contents_dir && zip -9 -o -r $zip *
-  cd $oldpwd
-}
+#   oldpwd=$PWD
+#   cd $new_contents_dir && zip -9 -o -r $zip *
+#   cd $oldpwd
+# }
 
+# create a new certificate for the given label using NSS's certutil
+# expected values for the label are 'trusted' and 'untrusted'
 create_cert_for_label()
 {
   label=$1
-  unsigned_zip=$2
-  out_signed_zip=$3
+
+  # initialize noise file
+  noisefile=$tmpdir/noise
+  head -c 32 /dev/urandom > $noisefile
+
+  # XXX: certutil cannot generate basic constraints without interactive prompts,
+  #      so we need to build response files to answer its questions
+  # XXX: certutil cannot generate AKI/SKI without interactive prompts so we just
+  #      skip them.
+  ca_responses=$tmpdir/ca_responses
+  ee_responses=$tmpdir/ee_responses  
+  echo y >  $ca_responses # Is this a CA?
+  echo >>   $ca_responses # Accept default path length constraint (no constraint)
+  echo y >> $ca_responses # Is this a critical constraint?
+  echo n >  $ee_responses # Is this a CA?
+  echo >>   $ee_responses # Accept default path length constraint (no constraint)
+  echo y >> $ee_responses # Is this a critical constraint?
 
   # XXX: We cannot give the trusted and untrusted versions of the certs the same
   # subject names because otherwise we'll run into
@@ -41,9 +57,11 @@ create_cert_for_label()
   ca_subj="CN=Examplla Root CA $label,OU=Examplla CA,$org"
   ee_subj="CN=Examplla Marketplace App Signing $label,OU=Examplla Marketplace App Signing,$org"
 
+  # create a database for this label using the password file
   db=$tmpdir/$label
   mkdir -p $db
   certutil -d $db -N -f $passwordfile
+
   make_cert="certutil -d $db -f $passwordfile -S -v 480 -g 2048 -Z SHA256 \
                       -z $noisefile -y 3 -2 --extKeyUsage critical,codeSigning"
   $make_cert -n ca1        -m 1 -s "$ca_subj" \
@@ -56,6 +74,8 @@ create_cert_for_label()
   certutil -d $db -L -n ee1 -r -o $db/ee1.der
 }
 
+# sign an app for the given label, input file, and output file.
+# assumes the global password file and cert database for that label still exist
 sign_app_with_cert_for_label()
 {
   label=$1
@@ -66,31 +86,21 @@ sign_app_with_cert_for_label()
   python sign_b2g_app.py -d $db -f $passwordfile -k ee1 -i $unsigned_zip -o $out_signed_zip -S test_app_identifier -V 1
 }
 
+# initialize temp directory
 rm -Rf $tmpdir
 mkdir $tmpdir
 
+# create password file
 echo password1 > $passwordfile
-head -c 32 /dev/urandom > $noisefile
 
-# XXX: certutil cannot generate basic constraints without interactive prompts,
-#      so we need to build response files to answer its questions
-# XXX: certutil cannot generate AKI/SKI without interactive prompts so we just
-#      skip them.
-echo y >  $ca_responses # Is this a CA?
-echo >>   $ca_responses # Accept default path length constraint (no constraint)
-echo y >> $ca_responses # Is this a critical constraint?
-echo n >  $ee_responses # Is this a CA?
-echo >>   $ee_responses # Accept default path length constraint (no constraint)
-echo y >> $ee_responses # Is this a critical constraint?
+# replace_zip $srcdir/unsigned.zip $srcdir/simple
 
-replace_zip $srcdir/unsigned.zip $srcdir/simple
+# create a trusted cert
+create_cert_for_label trusted
 
-# bwalker refactored sign_app_with_new_cert into create_cert_for_label and sign_app_with_cert_for_label
-
-create_cert_for_label trusted   $srcdir/unsigned.zip $srcdir/valid.zip
-# create_cert_for_label untrusted $srcdir/unsigned.zip $srcdir/unknown_issuer.zip
-
+# sign unsigned.zip from source dir using the trusted cert. Put the result in valid.zip
+# NOTA BENE: unsigned.zip must exist already
 sign_app_with_cert_for_label trusted   $srcdir/unsigned.zip $srcdir/valid.zip
-# sign_app_with_cert_for_label untrusted $srcdir/unsigned.zip $srcdir/unknown_issuer.zip
 
+# dump the trusted cert into a DER file
 certutil -d $tmpdir/trusted -f $passwordfile -L -n ca1 -r -o $srcdir/trusted_ca1.der
