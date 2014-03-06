56dc2686e74e4a2a4e4b2ed205479530f9516be0
Bill Walker <bwalker@mozilla.com>
2014-03-06 15:13:29 -0800

    get rid of labels, we only need the trusted one add configCertsDir for things we need to keep remove tmpdir when we're done with it

--- bin/generate.sh
+++ bin/generate.sh
@@ -12,19 +12,19 @@ set -x
 # let the source directory be the current working directory
 srcdir=$PWD
 
-# create a temp dir, put a password file in there
-tmpdir=$TMP/test_signed_apps
-passwordfile=$tmpdir/passwordfile
-
-# create a new certificate for the given label using NSS's certutil
-# expected values for the label are 'trusted' and 'untrusted'
-create_cert_for_label()
+# create a new certificate using NSS's certutil
+create_cert()
 {
-  label=$1
+  # initalize config/certs dir
+  configCertsDir=$1
 
   # initialize temp directory
+  tmpdir=$TMP/test_signed_apps
   rm -Rf $tmpdir
   mkdir $tmpdir
+
+  # password file persists in the configCerts dir
+  passwordfile=$configCertsDir/passwordfile
 
   # initialize noise file
   noisefile=$tmpdir/noise
@@ -50,11 +50,11 @@ create_cert_for_label()
   # subject names because otherwise we'll run into
   # SEC_ERROR_REUSED_ISSUER_AND_SERIAL.
   org="O=Examplla Corporation,L=Mountain View,ST=CA,C=US"
-  ca_subj="CN=Examplla Root CA $label,OU=Examplla CA,$org"
-  ee_subj="CN=Examplla Marketplace App Signing $label,OU=Examplla Marketplace App Signing,$org"
+  ca_subj="CN=Examplla Root CA,OU=Examplla CA,$org"
+  ee_subj="CN=Examplla Marketplace App Signing,OU=Examplla Marketplace App Signing,$org"
 
-  # create a database for this label using the password file
-  db=$tmpdir/$label
+  # create a database using the password file
+  db=$configCertsDir/trusted
   mkdir -p $db
   certutil -d $db -N -f $passwordfile
 
@@ -70,25 +70,36 @@ create_cert_for_label()
   certutil -d $db -L -n ee1 -r -o $db/ee1.der
 
   # dump the trusted cert into a DER file
-  certutil -d $tmpdir/trusted -f $passwordfile -L -n ca1 -r -o $srcdir/trusted_ca1.der  
+  certutil -d $db -f $passwordfile -L -n ca1 -r -o $configCertsDir/trusted_ca1.der  
+
+  # now that we're done with it, remove tmpdir
+  rm -Rf $tmpdir
 }
 
-# sign an app for the given label, input file, and output file.
-# assumes the global password file and cert database for that label still exist
-sign_app_with_cert_for_label()
+# sign an app for the input file and output file.
+# assumes the global password file and cert database still exist
+sign_app_with_cert()
 {
-  label=$1
+  # initalize config/certs dir
+  configCertsDir=$1
+
+  # password file persists in the configCerts dir
+  passwordfile=$configCertsDir/passwordfile
+
   unsigned_zip=$2
   out_signed_zip=$3
-  db=$tmpdir/$label
+
+  db=$configCertsDir/trusted
 
   python sign_b2g_app.py -d $db -f $passwordfile -k ee1 -i $unsigned_zip -o $out_signed_zip -S test_app_identifier -V 1
 }
 
+theConfigCertsDir=/Users/walker/mozilla/d2g/bin
+
 # create a trusted cert
-create_cert_for_label trusted
+create_cert $theConfigCertsDir
 
 # sign unsigned.zip from source dir using the trusted cert. Put the result in valid.zip
 # NOTA BENE: unsigned.zip must exist already
-sign_app_with_cert_for_label trusted $srcdir/unsigned.zip $srcdir/valid.zip
+sign_app_with_cert $theConfigCertsDir $srcdir/unsigned.zip $srcdir/valid.zip
 
