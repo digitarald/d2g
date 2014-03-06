f47c37f5101fb3b98e16914e8248511e3b5a4438
Austin King <shout@ozten.com>
2014-03-06 09:37:10 -0800

    Drop the db

--- scripts/clean.js
+++ scripts/clean.js
@@ -9,15 +9,4 @@ var User = require('../models/user');
 var Version = require('../models/version');
 
 console.log('Nuking everything... Good luck with that');
-
-var models = [Project, SignedPackage, User, Version];
-var total = models.length - 1;
-models.forEach(function(model, i) {
-        model.remove({}, function(err) {
-                total--;
-                console.log((i + 1) + '/' + models.length  + ': Mongo Collection removed');
-                if (0 === total) {
-                        mongoose.disconnect();
-                }
-        });
-});
+mongoose.connection.db.dropDatabase();
