bb8e0626f96bd56993508076efda7397f956cf75
Harald Kirschner <hkirschner@mozilla.com>
2014-05-01 09:49:23 -0700

    Fixed drop down behavior

--- public/js/components/nav.jsx
+++ public/js/components/nav.jsx
@@ -1,66 +1,116 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var cx = require('react/lib/cx.js');
 
 // var DropdownButton = require('react-bootstrap/dist/react-bootstrap').DropdownButton;
 
 
-var Component = React.createClass({
-
+var Nav = React.createClass({
 	getInitialState: function() {
 		return {
-			projectsOpen: false
+			projectsOpen: false,
+			userOpen: false,
+			burgerOpen: false
 		}
 	},
 
 	getDefaultProps: function() {
 		return {
 			projects: [],
-			project: -1
+			route: null,
+			routeParams: {}
 		};
 	},
 
-	handleDropdownClick: function(key) {
+	handleDropdown: function(key, event) {
+		var href = event.currentTarget.href;
+		if (href && href.substr(-1) == '#') {
+			event.preventDefault();
+		}
 		var set = {};
 		set[key] = !this.state[key];
 		this.setState(set);
 	},
 
 	render: function() {
+		var cls = {
+			user: {
+				dropdown: true,
+				open: this.state.userOpen
+			},
+			projects: {
+				dropdown: true,
+				active: (this.props.route == '/project/:id'),
+				open: this.state.projectsOpen
+			},
+			burger: {
+				'navbar-collapse': true,
+				collapse: !this.state.burgerOpen
+			},
+			new_project: {
+				active: (this.props.route == '/')
+			},
+			item: {
+				active: false
+			}
+		};
+
 		var currentProject;
+		var id = (this.props.route == '/project/:id') && this.props.params.id;
 		var projects = this.props.projects.map(function(project) {
-			if (this.props.project == project.key) {
+			var itemCls = {active: false}
+			if (project.key == id) {
 				currentProject = project.name;
+				itemCls.active = true;
 			}
 			var url = '#/project/' + project.key;
 			return (
-				<li><a href={url} onClick={this.handleDropdownClick.bind(this, 'projectsOpen')}>{project.name}</a></li>
+				<li key={project.key} className={cx(itemCls)}>
+					<a href={url}
+						onClick={this.handleDropdown.bind(this, 'projectsOpen')}>
+							{project.name}
+					</a>
+				</li>
 			);
 		}, this);
 		if (!currentProject) {
 			currentProject = 'Select a Project';
 		}
 
-		var dropdownClsProject = 'dropdown' + (this.state.projectsOpen ? ' open' : '');
-		var dropdownClsUser = 'dropdown' + (this.state.userOpen ? ' open' : '');
+		for (var key in cls) {
+			cls[key] = cx(cls[key]);
+		}
 
 		return (
 			<div className='navbar navbar-default navbar-fixed-top'>
 				<div className='container'>
 					<div className='navbar-header'>
-						<button type='button' data-toggle='collapse' data-target='.navbar-collapse' className='navbar-toggle'><span className='sr-only'>Toggle navigation</span><span className='icon-bar'></span><span className='icon-bar'></span><span className='icon-bar'></span></button><a href='/' className='navbar-brand'>D2G</a>
+						<button type='button' className='navbar-toggle'
+							onClick={this.handleDropdown.bind(this, 'burgerOpen')}>
+							<span className='sr-only'>Toggle navigation</span>
+							<span className='icon-bar'></span>
+							<span className='icon-bar'></span>
+							<span className='icon-bar'></span>
+						</button>
+						<a href='/' className='navbar-brand'>D2G</a>
 					</div>
-					<div className='collapse navbar-collapse'>
+					<div className={cls.burger}>
 						<ul className='nav navbar-nav'>
-							<li><a href='#/'>New Project</a></li>
-							<li className={dropdownClsProject}>
-								<a className='dropdown-toggle' onClick={this.handleDropdownClick.bind(this, 'projectsOpen')}>{currentProject} <span className='caret'></span></a>
-								<ul className='dropdown-menu open' ref='projects'>{projects}</ul>
+							<li className={cls.new_project}><a href='#/'>New Project</a></li>
+							<li className={cls.projects}>
+								<a className='dropdown-toggle' href='#'
+									onClick={this.handleDropdown.bind(this, 'projectsOpen')}>
+										{currentProject} <span className='caret'></span>
+								</a>
+								<ul className='dropdown-menu open' ref='projects'>
+									{projects}
+								</ul>
 							</li>
 						</ul>
 						<ul className='nav navbar-nav navbar-right'>
-							<li className={dropdownClsUser}>
-								<a href='#' data-toggle='dropdown' className='dropdown-toggle' onClick={this.handleDropdownClick.bind(this, 'userOpen')}>You <i className='caret'></i></a>
+							<li className={cls.user}>
+								<a href='#' className='dropdown-toggle' onClick={this.handleDropdown.bind(this, 'userOpen')}>You <i className='caret'></i></a>
 								<ul className='dropdown-menu'>
 									<li><a href='/account'>My Account</a></li>
 									<li className='divider'></li>
@@ -75,4 +125,4 @@ var Component = React.createClass({
 	}
 });
 
-module.exports = Component;
+module.exports = Nav;
